3e024caf20a0c335bb6e7dcc25681c99f4cd78c2
Hydrogenbear <hydrogenbear@poption.org>
2024-02-27 17:45:16 +0800

    simply fix errors

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -21,10 +21,15 @@ export function PostPreview({
   author,
   slug,
 }: Props) {
+  console.log("coverImage", coverImage)
   return (
     <div>
       <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
+        {coverImage ? (
+          <CoverImage slug={slug} title={title} src={coverImage} />
+        ) : (
+          <></>
+        )}
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
--- src/app/_components/track.tsx
+++ src/app/_components/track.tsx
@@ -1,5 +1,4 @@
 "use client"
 export function SectionSeparator() {
-  umami.track("event_name", [event_data]);
   return <hr className="border-neutral-200 mt-28 mb-24" />;
 }
